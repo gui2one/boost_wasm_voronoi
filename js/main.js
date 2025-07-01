d84221b406285a78bbc5d7723bca8c3b19bc5651
gui2one <guillaume.rouault.fx@gmail.com>
2025-07-01 20:03:53 +0200

    cleanup

--- js/main.js
+++ js/main.js
@@ -1,13 +1,12 @@
-import Foo from "./boost_voronoi.js";
+// @ts-ignore
+import Voronoi from "./boost_voronoi.js";
 
-let foo = await Foo();
+import { string_to_u8array } from "./utils.js";
+
+let voronoi = await Voronoi();
+let name = "master";
+const chars = string_to_u8array(name);
+voronoi.print_hello(chars);
 
 const points = new Float32Array([1.0, 2.0, 3.0, 4.0]);
-// const nBytes = points.length * points.BYTES_PER_ELEMENT;
-// const ptr = foo._malloc(nBytes);
-
-// foo.HEAPF32.set(points, ptr / points.BYTES_PER_ELEMENT);
-foo.set_data(points);
-// foo._free(ptr);
-
-console.log(foo);
+voronoi.set_data(points);
