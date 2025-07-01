2113e4ae8ee20644f89a035429c7a1ea118a514a
gui2one <guillaume.rouault.fx@gmail.com>
2025-07-01 20:03:33 +0200

    string_to_u8array

---
+++ js/utils.js
@@ -0,0 +1,9 @@
+function string_to_u8array(str) {
+  const buf = new Uint8Array(str.length);
+  for (let i = 0; i < str.length; i++) {
+    buf[i] = str.charCodeAt(i);
+  }
+  return buf;
+}
+
+export { string_to_u8array };
