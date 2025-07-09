32cccc7f90c664b3549367c85107f261a6622ce7
gui2one <guillaume.rouault.fx@gmail.com>
2025-07-10 00:50:42 +0200

    lost in la pampa !

---
+++ dist/test_module.js
@@ -0,0 +1,47 @@
+console.log("test_module.js");
+
+import { BuildDiagram, display_cells } from "../main.js";
+
+let canvas = document.createElement("canvas");
+
+canvas.width = 512;
+canvas.height = 512;
+let ctx = canvas.getContext("2d");
+document.body.appendChild(canvas);
+let coords = [];
+// for (let i = 0; i < 15; i++) {
+//   for (let j = 0; j < 15; j++) {
+//     coords.push(Math.random() * 512);
+//     coords.push(Math.random() * 512);
+//   }
+// }
+function add_point(x, y) {
+  coords.push(x);
+  coords.push(y);
+}
+
+function init() {
+  let diagram = BuildDiagram(coords);
+  console.log(diagram);
+  if (ctx === null) {
+    console.error("Rendering context is null");
+    return;
+  }
+  ctx?.clearRect(0, 0, canvas.width, canvas.height);
+  display_cells(diagram.cells, ctx);
+}
+init();
+
+// document.addEventListener("click", () => {
+//   console.clear();
+//   init();
+// });
+
+window.addEventListener("resize", () => {
+  init();
+});
+
+canvas.addEventListener("click", (e) => {
+  add_point(e.offsetX, e.offsetY);
+  init();
+});
